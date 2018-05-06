fe0d2c48ae28530f74f374e78937488c0a46ba44
geoffthemedio <geoffthemedio@gmail.com>
2018-05-06 18:43:38 +0200

    Added ShipDesignCost ComplexValueRef.

--- parse/Tokens.h
+++ parse/Tokens.h
@@ -418,6 +418,7 @@
 #define TOKEN_SEQ_13                            \
     (Shield)                                    \
     (ShipDesign)                                \
+    (ShipDesignCost)                            \
     (ShipDesignOrdering)                        \
     (ShipDesignsDestroyed)                      \
     (ShipDesignsLost)                           \
